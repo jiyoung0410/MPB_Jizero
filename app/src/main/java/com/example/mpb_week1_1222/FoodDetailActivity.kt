81f3eccd1c69dba7411bcfc7cb8b71bfa8a9fb4b
jiyoung <ghkd1310@naver.com>
2023-12-29 02:59:24 +0900

    food_detail_activity_add_dataclass

--- app/src/main/java/com/example/mpb_week1_1222/FoodDetailActivity.kt
+++ app/src/main/java/com/example/mpb_week1_1222/FoodDetailActivity.kt
@@ -8,12 +8,15 @@ import com.example.mpb_week1_1222.databinding.ActivityFooddetailBinding
 
 class FoodDetailActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityFooddetailBinding
-    private var originSacc: Int = 500
-    private var originProtein: Int = 100
-    private var originFat: Int = 200
-    private var originkcal: Int = 362
+    private lateinit var binding: ActivityFooddetailBinding
+    private lateinit var food: FoodItem
 
+    // Initial values for food properties
+    private val originSacc: Int = 500
+    private val originProtein: Int = 100
+    private val originFat: Int = 200
+    private val originkcal: Int = 362
+    private val foodName: String = "로제파스타"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,12 +24,19 @@ class FoodDetailActivity : AppCompatActivity() {
         binding = ActivityFooddetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.saccSizeTv.text = originSacc.toString() // 초기값 설정
-        binding.proteinSizeTv.text = originProtein.toString() // 초기값 설정
-        binding.fatSizeTv.text = originFat.toString() // 초기값 설정
-        binding.foodKcalNumTv.text = originkcal.toString() // 초기값 설정
+        // Replace this with the actual food name
+        //val foodName = "로제파스타"
+        // Initialize food details using the data class
+        food = FoodItem(foodName, originSacc, originProtein, originFat, originkcal)
 
+        // Set initial UI values based on food details
+        binding.foodNameTv.text = food.name.toString()
+        binding.saccSizeTv.text = food.saccSize.toString()
+        binding.proteinSizeTv.text = food.proteinSize.toString()
+        binding.fatSizeTv.text = food.fatSize.toString()
+        binding.foodKcalNumTv.text = food.kcal.toString()
 
+        // Minus button click listener
         binding.minusTv.setOnClickListener {
             val text = binding.gramTv.text.toString().trim()
             val numericText = text.filter { it.isDigit() }
@@ -35,37 +45,48 @@ class FoodDetailActivity : AppCompatActivity() {
                 var currentValue = numericText.toInt()
 
                 if (currentValue >= 100) {
+                    // Update food properties when reducing quantity
                     currentValue -= 100
-                    binding.saccSizeTv.text = (binding.saccSizeTv.text.toString().toInt() - originSacc).toString() // 변경된 값으로 설정
-                    binding.proteinSizeTv.text = (binding.proteinSizeTv.text.toString().toInt() - originProtein).toString() // 변경된 값으로 설정
-                    binding.fatSizeTv.text = (binding.fatSizeTv.text.toString().toInt() - originFat).toString() // 변경된 값으로 설정
-                    binding.foodKcalNumTv.text = (binding.foodKcalNumTv.text.toString().toInt() - originkcal).toString() // 변경된 값으로 설정
-                    binding.gramTv.text = "$currentValue g"
+                    food.saccSize -= originSacc
+                    food.proteinSize -= originProtein
+                    food.fatSize -= originFat
+                    food.kcal -= originkcal
+
+                    // Update UI with new values
+                    updateUI(currentValue)
                 } else {
                     Toast.makeText(this, "0보다 작을 수 없습니다.", Toast.LENGTH_SHORT).show()
                 }
-            } else {
-                Toast.makeText(this, "fff보다 작을 수 없습니다.", Toast.LENGTH_SHORT).show()
             }
-
         }
 
+        // Plus button click listener
         binding.plusTv.setOnClickListener {
             val text = binding.gramTv.text.toString().trim()
             val numericText = text.filter { it.isDigit() }
 
-
             if (numericText.isNotEmpty()) {
                 var currentValue = numericText.toInt()
-
                 currentValue += 100
-                binding.saccSizeTv.text = (binding.saccSizeTv.text.toString().toInt() + originSacc).toString() // 변경된 값으로 설정
-                binding.proteinSizeTv.text = (binding.proteinSizeTv.text.toString().toInt() + originProtein).toString() // 변경된 값으로 설정
-                binding.fatSizeTv.text = (binding.fatSizeTv.text.toString().toInt() + originFat).toString() // 변경된 값으로 설정
-                binding.foodKcalNumTv.text = (binding.foodKcalNumTv.text.toString().toInt() + originkcal).toString() // 변경된 값으로 설정
-                binding.gramTv.text = "$currentValue g"
 
+                // Update food properties when increasing quantity
+                food.saccSize += originSacc
+                food.proteinSize += originProtein
+                food.fatSize += originFat
+                food.kcal += originkcal
+
+                // Update UI with new values
+                updateUI(currentValue)
             }
         }
+    }
+
+    // Function to update the UI with new values
+    private fun updateUI(currentValue: Int) {
+        binding.saccSizeTv.text = food.saccSize.toString()
+        binding.proteinSizeTv.text = food.proteinSize.toString()
+        binding.fatSizeTv.text = food.fatSize.toString()
+        binding.foodKcalNumTv.text = food.kcal.toString()
+        binding.gramTv.text = "$currentValue g"
     }
 }
---
+++ app/src/main/java/com/example/mpb_week1_1222/FoodItem.kt
@@ -0,0 +1,9 @@
+package com.example.mpb_week1_1222
+
+data class FoodItem(
+    var name: String,
+    var saccSize: Int,
+    var proteinSize: Int,
+    var fatSize: Int,
+    var kcal: Int
+)
